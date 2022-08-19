95caa0e606bb7bad3bc7bde4f1f693380f16e0a3
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-08-19 15:41:13 +0900

    chore: README.md 작성

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,14 @@ module.exports = {
   extends: ["airbnb", "prettier"],
   plugins: ["json-format", "simple-import-sort"],
   rules: {
-    "no-undef": "off",
-    "no-unused-vars": "off",
-    "no-underscore-dangle": "off",
-    "no-nested-ternary": "off",
-    "no-shadow": "off",
-    "react/no-danger": "off",
+    // custom
+    "react/function-component-definition": 0,
+    "react/react-in-jsx-scope": 0,
+
     "react/jsx-filename-extension": [
       1,
       { extensions: [".js", ".jsx", ".tsx"] }
     ],
-    "react/function-component-definition": "off",
-    "import/prefer-default-export": "off",
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/extensions": "off",
-    "react/require-default-props": "off",
-    "react/destructuring-assignment": "off",
-    "react/prop-types": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/react-in-jsx-scope": "off",
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
     "jsx-a11y/anchor-is-valid": [
